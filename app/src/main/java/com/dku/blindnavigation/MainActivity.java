341e82af9b6be0516e9d7ffd7ff15e7b6a8d524d
SyuA0529 <silbi3408@gmail.com>
2023-05-02 21:46:02 +0900

    Feat: 위치 권한(COARSE, FIND, BACKGROUND) 요청 기능 구현 완료
    위치 권한(COARSE, FIND, BACKGROUND) 요청 기능 구현 완료 및 테스트 Activity 구현
    
    Resolve: #4

--- app/src/main/java/com/dku/blindnavigation/MainActivity.java
+++ app/src/main/java/com/dku/blindnavigation/MainActivity.java
@@ -1,14 +1,17 @@
 package com.dku.blindnavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button locationTestButton = findViewById(R.id.locationTestButton);
+        locationTestButton.setOnClickListener(v -> startActivity(new Intent(this, TestLocationActivity.class)));
     }
 }
